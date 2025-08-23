6257dde1fc34586bc07df60ac103996319889472
mandar00 <sakpalm466@gmail.com>
2025-08-23 19:31:17 +0530

    clean start spotify clone

--- app/(root)/create-podcast/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreatePodCast=() =>{
-return(
-    <>
-      
-    </>)
-}
-export default CreatePodCast
--- app/(root)/podcasts/[podcastId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PodCastDetails=({params}:{params:{podcastId:string}}) =>{
-return(
-    <>
-      
-    </>)
-}
-export default PodCastDetails

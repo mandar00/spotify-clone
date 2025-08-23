76dfcfeb106053836a0ac23b96e8a8f3dc60931d
mandar00 <sakpalm466@gmail.com>
2025-08-23 12:09:46 +0530

    created left nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,24 @@
+import LeftSidebar from "@/components/LeftSidebar";
 import React from "react";
+import "../globals.css"
+import { Metadata } from "next";
 
+export const metadata :Metadata={
+  title:"Podcaster",
+  description:"Generate your own PodCast using AI",
+  icons:{icon:"/icons/svg/logo.svg"}
+}
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      <aside>Left sidebar</aside>
-      <div>{children}</div>
-      <aside>Right Sidebar</aside>
+    <div className="flex bg-black-3">
+      <LeftSidebar/>
+      <section className="flex min-h-screen flex-1 p-2 flex-col">
+        <div className="">
+          
+        {children}
+        </div>
+        </section>
+      <aside className="text-white-1">Right Sidebar</aside>
     </div>
   );
 };
